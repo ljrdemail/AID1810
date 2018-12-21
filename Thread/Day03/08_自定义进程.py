c827eabfe3c07fcd71065bf667e37a00bc5b7b29
root <root>
2018-12-21 15:52:52 +0800

    sdsd

--- Thread/Day03/08_自定义进程.py
+++ Thread/Day03/08_自定义进程.py
@@ -9,17 +9,23 @@ class MyProcess(Process):
         super(MyProcess, self).__init__()
         self.n = n
 
-    # 重写run方法
-    def run(self):
+    def fun1(self):
         for i in range(self.n):
             print("子进程在做事！")
             time.sleep(1)
+
+    # 不用调用父类的run方法 因为要的就是覆盖
+
+    # 重写run方法(名字不能变) 父类中对此方法做了自动化处理
+    # 如果换了方法名 start就找不到了 所以不能改名
+    def run(self):
+        self.fun1()
 
 
 if __name__ == "__main__":
     start = time.time()
     p = MyProcess(3)
-    p.start()
+    p.start()  # start 去调用run 方法
     p.join()
     end = time.time()
     print("我是父进程")
---
+++ Thread/Day03/exec01_counddown.py
@@ -0,0 +1,27 @@
+import time
+from multiprocessing import Process
+
+
+class MyCountDown(Process):
+    def __init__(self, n):
+        self.n = n
+        super(MyCountDown, self).__init__()
+
+    def fun1(self):
+        print("倒计时%d秒开始" % self.n)
+        for i in range(self.n, 0, -1):
+            print(i)
+            time.sleep(1)
+        print("时间到！！")
+
+    def run(self):
+        self.fun1()
+
+
+if __name__ == "__main__":
+    start = time.time()
+    p = MyCountDown(10)
+    p.start()
+    p.join()
+    end = time.time()
+    print("程序执行了%.2f秒" % (end - start))
