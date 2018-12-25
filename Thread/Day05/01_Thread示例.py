1323160d668cc094a18075923d280ac604bfbf09
root <root>
2018-12-25 21:10:34 +0800

    sdsd

--- Thread/Day05/01_Thread示例.py
+++ Thread/Day05/01_Thread示例.py
@@ -7,7 +7,7 @@ from threading import Thread
 def music():
     for i in range(2):
         time.sleep(2)
-        print("播放怒放的生命", os.getpid())#赋值线程进程号都是一个 因为共享线程资源
+        print("播放怒放的生命", os.getpid())#父子进程号都是一个 因为共享线程资源
 
 
 if __name__ == "__main__":
@@ -19,12 +19,12 @@ if __name__ == "__main__":
     # 主线程
     for i in range(2):
         time.sleep(3)
-        print("播放学猫叫", os.getpid())#赋值线程进程号都是一个 因为共享线程资源
+        print("播放学猫叫", os.getpid())#父子进程号都是一个 因为共享线程资源
 
 
 
     t.join()
     # for i in range(2):
     #     time.sleep(3)
-    #     print("播放学猫叫", os.getpid())  # 赋值线程进程号都是一个 因为共享线程资源
+    #     print("播放学猫叫", os.getpid())  # 父子进程号都是一个 因为共享线程资源
     # 如果主进程放在join下面 name 子进程打印完之后才到主进程
--- Thread/Day05/02_全局变量.py
+++ Thread/Day05/02_全局变量.py
@@ -9,7 +9,7 @@ n = 1
 def music():
     for i in range(2):
         time.sleep(2)
-        print("播放怒放的生命", os.getpid())  # 赋值线程进程号都是一个 因为共享线程资源
+        print("播放怒放的生命", os.getpid())  # 父子进程号都是一个 因为共享线程资源
     # 在子线程中更改变量
     global n
     n = 1000
@@ -25,12 +25,12 @@ if __name__ == "__main__":
     # 主线程
     # for i in range(2):
     #     time.sleep(3)
-    #     print("播放学猫叫", os.getpid())#赋值线程进程号都是一个 因为共享线程资源
+    #     print("播放学猫叫", os.getpid())#父子进程号都是一个 因为共享线程资源
 
     t.join()
 
     for i in range(2):
         time.sleep(3)
-        print("播放学猫叫", os.getpid())  # 赋值线程进程号都是一个 因为共享线程资源
-    print("主线程中n的值：", n) #线程共享进程的资源 线程 修改了 别的线程会受到影响 类似于共享内存
+        print("播放学猫叫", os.getpid())  #父子进程号都是一个 因为共享线程资源
+    print("主线程中n的值：", n) #线程共享进程的资源 一个线程 修改了 别的线程会受到影响 类似于共享内存
     # 如果主进程放在join下面 name 子进程打印完之后才到主进程
