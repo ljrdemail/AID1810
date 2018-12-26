24278fae796e82acbd44c16548012ecb7a1064b9
root <root>
2018-12-26 17:22:00 +0800

    sdsd

--- Thread/Day03/07_进程对象属性.py
+++ Thread/Day03/07_进程对象属性.py
@@ -1,5 +1,7 @@
+import os
 import time
 from multiprocessing import Process
+from multiprocessing import current_process
 
 
 # 写函数，每隔一秒打印当前时间，打印N次
@@ -11,8 +13,9 @@ def timming(n):
 
 
 # 创建进程
-p = Process(target=timming, args=(7,), name="计时器") #只是创建了对象 只有等到start 才开始分配资源执行
+p = Process(target=timming, args=(7,), name="计时器")  # 只是创建了对象 只有等到start 才开始分配资源执行
 print(p.name)  # 默认 process -1
+
 # print(p.pid)
 # print(p.is_alive())
 # print(p.daemon)
@@ -20,9 +23,12 @@ print(p.name)  # 默认 process -1
 # 启动进程
 p.daemon = True  # 设置为守护进程 主进程结束 子进程跟着结束 要在start之前设定 要不然人家都启动了 玩毛
 p.start()
+
 # 进程创建起来之后才能有这些属性
 print(p.pid)
 print(p.is_alive())
+print("通过current_process获取主进程pid:", current_process().pid)
+print(os.getpid())
 print(p.daemon)
 
 # 回收进程
