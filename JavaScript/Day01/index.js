839dd8aeee1a1f6eb28cd9af625c039f53cce122
root <root>
2019-01-07 11:53:08 +0800

    sdsd

---
+++ JavaScript/Day01/index.js
@@ -0,0 +1,5 @@
+prompt("请输入姓名：")
+//prompt带有输入框的弹框 alert是普通弹框 同样也是阻塞函数
+document.write("<strong>动态添加</strong>");
+//向页面中添加内容，根据书写位置 添加在body中显示 这里就是放在body第一行 因为应该是写在Head中（因为代码在head中引入） 但是由于head渲染不了
+//所以放在body中第一行
