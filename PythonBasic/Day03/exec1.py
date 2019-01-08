43a8991a00ff92f958079563dc7c5e523b24124b
root <root>
2019-01-08 21:32:05 +0800

    sdsd

--- PythonBasic/Day03/exec1.py
+++ PythonBasic/Day03/exec1.py
@@ -1,12 +1,26 @@
-line = int(input("请输入矩形的宽度:"))
+# 练习:
+#   写一个程序,打印一个高度为4行的矩形方框
+#     要求输入一个整数,此整数代表矩形的宽度,输出此矩形:
+#   如:
+#     请输入矩形的宽度: 10
+#   打印如下:
+#     ##########
+#     #        #
+#     #        #
+#     ##########
 
-print("#" * line)
-if (line == 1):
-    print("#")
-    print("#")
-    print("#" * line)
+width = int(input("请输入矩形宽度: "))
+line1 = "#" * width
+print(line1)
+
+if width == 1:
+    line2 = '#'
 else:
-    print("#", " " * (line - 2), "#", sep="")  # 把自动空的去掉
-    print("#", " " * (line - 2), "#", sep="")  # 把自动空的去掉
-    print("#" * line)
+    line2 = '#' + ' ' * (width - 2) + '#'
+
+print(line2)
+print(line2)
+print(line1)
+
+
 
