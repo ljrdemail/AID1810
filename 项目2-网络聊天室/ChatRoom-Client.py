15c6e7a64c495da65fb92a72c3d686eab8068727
root <root>
2019-01-17 19:52:46 +0800

    sdsd

--- 项目2-网络聊天室/ChatRoom-Client.py
+++ 项目2-网络聊天室/ChatRoom-Client.py
@@ -3,14 +3,13 @@ import sys
 from socket import *
 
 
-# 创建套接字函数
 
+# 子进程发消息函数
 def sendMsg(client,name,addr):
     #子进程发消息给服务端 服务端 再进行转发 除他自己
     while True:
-        content=input("请发言(quit退出):\n")
-        print("\n")
-        if(content=="quit"):
+        content=input("请发言(quit退出):\n") #加一个空行避免 别的行打印到这一样
+        if(content=="quit" or content=="exit"):
             message="Q "+name
             client.sendto(message.encode(),addr)
             sys.exit("退出聊天室")#子进程退出
@@ -19,41 +18,42 @@ def sendMsg(client,name,addr):
         message="C %s %s" %(name,content)
         client.sendto(message.encode(),addr)
 
-
+# 父进程收消息函数
 def recvMsg(client):
     while True:
         message,addr=client.recvfrom(1024)
+        # 父进程退出
         if (message.decode() == "EXIT"):
             sys.exit("成功退出！")
         print(message.decode())
 
 
-
+    # 创建套接字函数
 def main():
+
+    # 创建UDP套接字
     client = socket(AF_INET, SOCK_DGRAM)
     client.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-    # ip = input("请输入IP：")
-    # port = input("请输入端口：")
-    # address = (ip, int(port))
+
     if len(sys.argv)<3:
         print('参数错误')
         return
     address=(sys.argv[1],int(sys.argv[2]))
-    print(address)
 
-    # 发消息
-    # client.sendto("L 金毛狮王".encode(),address)
     while True:
         name = input("请输入姓名：")
         message = "L " + name;
+        # 向服务器发送消息
         client.sendto(message.encode(), address)
         # 接收服务器反馈
         data, addr = client.recvfrom(1024)
         if (data.decode() == "OK"):
             print("您已进入聊天室")
+            #退出循环 不再输入用户名
             break
         else:
-            print(data.decode())  # 打印不允许进入的原因
+            print(data.decode())  # 打印从服务器发过来的不允许进入的原因
+
     # 创建进程 子进程发消息 父进程收消息
     pid = os.fork()
     if pid < 0:
--- 项目2-网络聊天室/ChatRoom-Server.py
+++ 项目2-网络聊天室/ChatRoom-Server.py
@@ -2,38 +2,41 @@ import os
 from socket import *
 
 
-# 创建网络连接
+
+# 进入到聊天室请求处理函数
 def doLogin(server, user, name, addr):
-    # 进入到聊天室请求处理函数
-    print(name in user)
-    if (name in user) or name == "管理员":
-        server.sendto("该用户已经存在".encode(), addr)
+
+
+    if (name in user) or name == "管理员":#不能叫管理员
+        server.sendto("该用户已经存在或使用了非法名字（管理员）".encode(), addr)
         return
-    # 名字不存在发送OK给客户端
-    server.sendto("OK".encode(), addr)
+    # 名字不存在，发送OK给客户端
+    server.sendto("OK".encode(), addr) #addr客户端的地址
     # 通知其他人 这人已经登录
     message = '欢迎 %s 进入聊天室' % name
     for u in user:
         server.sendto(message.encode(), user[u])
+    # 加入存储结构user中
     user[name] = addr
-    print(user)
 
-
+# 聊天函数（把消息发给其他成员）
 def doChat(server, content, user, name):
+    # 包装消息
     msg = name + "说：" + content
+    # 发给其他成员
     for u in user:
         if (u != name):
             server.sendto(msg.encode(), user[u])
 
-
+# 处理客户端退出函数
 def doQuit(server, name, user):
     # 通知其他成员
     message = name + "退出了聊天室！"
     for u in user:
-        if (u != name):
+        if (u != name): #排除退出者自己
             server.sendto(message.encode(), user[u])
         else:
-            # 给退出者本人发消息
+            # 给退出者本人发消息 用于客户端收到信息之后干掉进程
             server.sendto(b'EXIT', user[name])
     # 从存储结构中删除
     del user[name]
@@ -44,11 +47,11 @@ def doRequest(server):
     # 收消息
     while True:
         message, addr = server.recvfrom(1024)
-        # [L,"金毛狮王"]
+        #msgList: ['L', '金毛狮王']
         meglist = message.decode().split(" ")  # 默认是空白作为分隔符
-        print(meglist)
         if (meglist[0] == "L"):
             doLogin(server, user, meglist[1], addr)
+            # msgList: ['C','name','你好','步惊云']
         if (meglist[0] == "C"):
             content = ' '.join(meglist[2:])
             # 发给其他所有成员
@@ -58,18 +61,22 @@ def doRequest(server):
 
 
 def main(clientHandler=None):
+    # 创建网络连接
+    # 创建UDP套接字
     server = socket(AF_INET, SOCK_DGRAM)
+    # 设置端口复用
     server.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+    # 绑定地址
     server.bind(("0.0.0.0", 9999))
 
-    # 只进程负责管理员喊话，父进程负责和客户端交互
+    # 创建多进程，子进程负责管理员喊话，父进程和客户端交互
 
     pid = os.fork()
     if pid < 0:
         print('进程创建失败')
         return
     if pid == 0:
-        # 子进程负责喊话
+        # 子进程负责管理员发送消息
         # print('负责管理员喊话的进程')
         pass
 
