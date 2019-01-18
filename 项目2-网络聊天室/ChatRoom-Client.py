6db0a66bba1f578d85b2843c7569b2cd3b9863fc
root <root>
2019-01-18 10:16:19 +0800

    sdsd

--- 项目2-网络聊天室/ChatRoom-Client.py
+++ 项目2-网络聊天室/ChatRoom-Client.py
@@ -3,42 +3,50 @@ import sys
 from socket import *
 
 
-
 # 子进程发消息函数
-def sendMsg(client,name,addr):
-    #子进程发消息给服务端 服务端 再进行转发 除他自己
+def sendMsg(client, name, addr):
+    # 子进程发消息给服务端 服务端 再进行转发 除他自己
     while True:
-        content=input("请发言(quit退出):\n") #加一个空行避免 别的行打印到这一样
-        if(content=="quit" or content=="exit"):
-            message="Q "+name
-            client.sendto(message.encode(),addr)
-            sys.exit("退出聊天室")#子进程退出
+        try:
+            content = input("请发言(quit退出):")  # 加一个空行避免 别的行打印到这一样
+        except KeyboardInterrupt:
+            content = "quit" #让它走正常退出流程
 
-        #包装消息
-        message="C %s %s" %(name,content)
-        client.sendto(message.encode(),addr)
+            if (content == "quit" or content == "exit"):
+                message = "Q " + name
+                client.sendto(message.encode(), addr)
+                os._exit(0)  # 子进程退出
+
+            # 包装消息
+            message = "C %s %s" % (name, content)
+            client.sendto(message.encode(), addr)
+
 
 # 父进程收消息函数
 def recvMsg(client):
     while True:
-        message,addr=client.recvfrom(1024)
+        try:
+         message, addr = client.recvfrom(1024)
+        except KeyboardInterrupt:
+            sys.exit("退出聊天室") #因为公用终端 子进程挂了 这边也受到影响
         # 父进程退出
         if (message.decode() == "EXIT"):
             sys.exit("成功退出！")
-        print(message.decode())
-
+            # 因为print()覆盖了之前的input()界面 在这里再次输出一次
+        print(message.decode() + "\n请发言(quit退出):", end='')
 
     # 创建套接字函数
-def main():
 
+
+def main():
     # 创建UDP套接字
     client = socket(AF_INET, SOCK_DGRAM)
     client.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
 
-    if len(sys.argv)<3:
+    if len(sys.argv) < 3:
         print('参数错误')
         return
-    address=(sys.argv[1],int(sys.argv[2]))
+    address = (sys.argv[1], int(sys.argv[2]))
 
     while True:
         name = input("请输入姓名：")
@@ -49,7 +57,7 @@ def main():
         data, addr = client.recvfrom(1024)
         if (data.decode() == "OK"):
             print("您已进入聊天室")
-            #退出循环 不再输入用户名
+            # 退出循环 不再输入用户名
             break
         else:
             print(data.decode())  # 打印从服务器发过来的不允许进入的原因
@@ -60,7 +68,7 @@ def main():
         sys.exit("创建进程失败")
         # 子进程发消息
     elif pid == 0:
-        sendMsg(client,name, addr)
+        sendMsg(client, name, addr)
     else:
         # 父进程收消息
         recvMsg(client)
--- 项目2-网络聊天室/ChatRoom-Server.py
+++ 项目2-网络聊天室/ChatRoom-Server.py
@@ -2,38 +2,38 @@ import os
 from socket import *
 
 
-
 # 进入到聊天室请求处理函数
 def doLogin(server, user, name, addr):
-
-
-    if (name in user) or name == "管理员":#不能叫管理员
+    if (name in user) or name == "管理员":  # 不能叫管理员
         server.sendto("该用户已经存在或使用了非法名字（管理员）".encode(), addr)
         return
     # 名字不存在，发送OK给客户端
-    server.sendto("OK".encode(), addr) #addr客户端的地址
+    server.sendto("OK".encode(), addr)  # addr客户端的地址
     # 通知其他人 这人已经登录
-    message = '欢迎 %s 进入聊天室' % name
+    message = '\n欢迎 %s 进入聊天室' % name
     for u in user:
         server.sendto(message.encode(), user[u])
     # 加入存储结构user中
     user[name] = addr
 
+
 # 聊天函数（把消息发给其他成员）
 def doChat(server, content, user, name):
     # 包装消息
-    msg = name + "说：" + content
+
+    msg ='\n%s说:%s' %(name,content)
     # 发给其他成员
     for u in user:
-        if (u != name):
+        if (u != name):  # 如果是管理员 这条永远成立 因为 列表中不可能有管理员 因为登录的时候要求neme!=管理员
             server.sendto(msg.encode(), user[u])
+
 
 # 处理客户端退出函数
 def doQuit(server, name, user):
     # 通知其他成员
-    message = name + "退出了聊天室！"
+    message = "\n"+name + "退出了聊天室！"
     for u in user:
-        if (u != name): #排除退出者自己
+        if (u != name):  # 排除退出者自己
             server.sendto(message.encode(), user[u])
         else:
             # 给退出者本人发消息 用于客户端收到信息之后干掉进程
@@ -47,7 +47,7 @@ def doRequest(server):
     # 收消息
     while True:
         message, addr = server.recvfrom(1024)
-        #msgList: ['L', '金毛狮王']
+        # msgList: ['L', '金毛狮王']
         meglist = message.decode().split(" ")  # 默认是空白作为分隔符
         if (meglist[0] == "L"):
             doLogin(server, user, meglist[1], addr)
@@ -57,7 +57,7 @@ def doRequest(server):
             # 发给其他所有成员
             doChat(server, content, user, meglist[1])
         if (meglist[0] == "Q"):
-            doQuit(server,meglist[1],user)
+            doQuit(server, meglist[1], user)
 
 
 def main(clientHandler=None):
@@ -78,7 +78,11 @@ def main(clientHandler=None):
     if pid == 0:
         # 子进程负责管理员发送消息
         # print('负责管理员喊话的进程')
-        pass
+        content = input("管理员消息:")
+        # 对消息进行封装
+        message = 'C 管理员 ' + content
+        # 父进程在监听，把消息发送给父进程
+        server.sendto(message.encode(), ("127.0.0.1", 9999))
 
     else:
         # 父进程负责处理客户端的请求
