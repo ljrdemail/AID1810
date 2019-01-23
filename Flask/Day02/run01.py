73928e5fb1c593bf77ca907b0411325f9f06ab5c
root <root>
2019-01-23 20:42:33 +0800

    sdsd

--- Flask/Day02/run01.py
+++ Flask/Day02/run01.py
@@ -11,11 +11,11 @@ def var_view():
     # 在这个视图处理函数中渲染到01-war.html模板上
     # 渲染的参数1 :name 值为wangwc
     # 渲染的参数2: age  值为37
-    return render_template("Day2 01-var.html", name="李嘉睿", age=37)
+    return render_template("Day 02_01-var.html", name="李嘉睿", age=37)
 
 
 @app.route('/02-var')
-# 目的：验证允许放在模板中作为变量的数据类型
+# 目的:验证允许放在模板中作为变量的数据类型
 def var02():
     # 字符串
     name = "wangwc"
@@ -33,20 +33,25 @@ def var02():
     params = locals()  # 所有局部变量 不用一个个的打了
     print(locals())
 
-    # return render_template("Day 02_02-var.html", name=name, age=age, tup=tup, list=list, dict=dict, person=person)
-    # return render_template("Day 02_03-var.html", params=params)
-    return render_template("Day 02_03-var.html", params=params)
+    # return render_template("Day 02_02-var.html", name=name, age=age, tup=tup, list=list, dict=dict, person=person) #一个个的返回
+    # return render_template("Day 02_03-var.html", params=params)#写成字典返回整个字典
+    return render_template("Day 02_03-var.html", params=params)  # 通过locals() 获取字典返回
 
 
+# localhost:5000/03-if
+# 目的:使用if标签
 @app.route('/03-if')
 def if_view():
     name = "王老师"
     age = 61
     return render_template("Day02_04_if.html", params=locals())
+    #需要注意的是html 中params.name 不用{{}} endif 不要忘了写
 
 
 # 先把模板交给python解释器 解释这些标签 然后再交给浏览器渲染 所以可以动静结合
 
+# localhost:8000/04-for
+#目的:for标签的使用
 @app.route("/04-for")
 def for_view():
     list = ["李嘉睿", "潘仁晓", "李幼兰", "黄翠英", "潘华", "潘仁娟", "李昮"]
@@ -54,6 +59,8 @@ def for_view():
     return render_template("Day02_05_for.html", params=locals())
 
 
+# localhost:5000/05-macro
+#目的:熟悉宏(macro)的声明和使用
 # 熟悉宏的声明和使用
 @app.route("/05-marco")
 def macro_view():
@@ -61,6 +68,8 @@ def macro_view():
     return render_template("Day02_06_marco.html", params=locals())
 
 
+# localhost:5000/06-static
+#目的:熟悉静态文件的使用
 # 熟悉静态文件的使用
 @app.route('/06-static')
 def static_view():
