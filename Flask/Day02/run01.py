633dba17b6421d52510adbbbfc5436f57ecc7f32
root <root>
2019-01-23 15:20:41 +0800

    sdsd

--- Flask/Day02/run01.py
+++ Flask/Day02/run01.py
@@ -25,12 +25,36 @@ def var02():
     tup = ('抽烟', '喝酒', '保健')
     list = ['赵丽颖', '赵蒙蒙', '葛老师']
     dict = {"WFR": "夫人：王夫人", 'LXG': "儿子:李小超", 'LEG': "邻居：李二狗"}
-    p = person()
-    p.name = "王伟超老师"
-    return render_template("Day 02_02-var.html", name=name, age=age, tup=tup, list=list, dict=dict, person=p)
+    person = Person()
+    person.name = "王伟超老师"
+
+    # params = {"name": name, "age": age, "tup": tup, "list": list, "dict": dict, "person": person}
+
+    params = locals()  # 所有局部变量 不用一个个的打了
+    print(locals())
+
+    # return render_template("Day 02_02-var.html", name=name, age=age, tup=tup, list=list, dict=dict, person=person)
+    # return render_template("Day 02_03-var.html", params=params)
+    return render_template("Day 02_03-var.html", params=params)
 
 
-class person(object):
+@app.route('/03-if')
+def if_view():
+    name = "王老师"
+    age = 61
+    return render_template("Day02_04_if.html", params=locals())
+
+
+# 先把模板交给python解释器 解释这些标签 然后再交给浏览器渲染 所以可以动静结合
+
+@app.route("/04-for")
+def for_view():
+    list = ["李嘉睿", "潘仁晓", "李幼兰", "黄翠英", "潘华", "潘仁娟", "李昮"]
+    dict = {"SWK": "孙悟空", "PJL": "潘金莲", "GY": "关羽", "LLL": "刘姥姥"}
+    return render_template("Day02_05_for.html", params=locals())
+
+
+class Person(object):
     name = None;
 
     def say(self):
