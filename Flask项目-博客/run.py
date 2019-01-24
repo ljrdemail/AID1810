de86beaef3fc986af4325b7a0bda458550f08194
root <root>
2019-01-24 19:12:36 +0800

    sdsd

--- Flask项目-博客/run.py
+++ Flask项目-博客/run.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask import render_template
 
 app = Flask(__name__)
@@ -12,6 +12,34 @@ def index():
 @app.route("/list")
 def list():
     return render_template('list.html')
+
+
+@app.route("/login", methods=["POST", "GET"])
+def longin():
+    method = request.method
+    if method == "GET":
+        return render_template("/login.html")
+    else:
+        uname = request.form.get("username")
+        pwd = request.form.get("password")
+        print(uname, pwd)
+        return ""
+
+
+@app.route("/register", methods=["POST", "GET"])
+def register():
+    method = request.method
+    if method == "GET":
+        return render_template("/register.html")
+    else:
+        username = request.form.get("username")
+        email = request.form.get("email")
+        url = request.form.get("url")
+        password = request.form.get("password")
+
+        print(username, email, url, password)
+        return ""
+
 
 
 if __name__ == "__main__":
