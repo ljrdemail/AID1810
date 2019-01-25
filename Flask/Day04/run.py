c6055ce21525c515790415043c2d0da05675b48c
root <root>
2019-01-25 15:13:17 +0800

    sdsd

--- Flask/Day04/run.py
+++ Flask/Day04/run.py
@@ -1,3 +1,6 @@
+import datetime
+import os
+
 from flask import Flask, request, render_template
 
 app = Flask(__name__)
@@ -29,11 +32,48 @@ def file_view():
         uimg = request.files['uimg']  # uimg是一个文件 传入'uimg' 用于传入name=uimg的那个文本框
         # 保存数据 相对路径satic中
         try:
-            uimg.save("static/" + uimg.filename)
+            # 以上传的时间来作为上传的文件名-避免上传的资源重复
+            # 格式 YYYYMMDDHHMMSSFFFFFF.扩展名
+            # 根据时间平街层名称字符串
+            # 根据原有的文件名获取扩展名
+            ftime = datetime.datetime.now().strftime("%Y%m%d%H%M%S%f")
+            houzhui = uimg.filename.split(".")[-1]
+            houzhui2 = uimg.filename[uimg.filename.index(".") + 1::]
+
+            # 拼接
+            # filename=ftime+"."+houzhui
+            filename = ftime + "." + houzhui2
+            # uimg.save("static/" + filename)
+            # 使用绝对路径
+            basedir = os.path.dirname(__file__)  # run.py所在的路径
+            # 拼接完整路径
+            uploadpath = os.path.join(basedir, "static", filename)
+            uimg.save(uploadpath)
         except Exception as e:
             print(e)
             return "上传失败！"
     return "上传文件成功"
+
+
+@app.route("/03-release", methods=["GET", "POST"])
+def release_view():
+    method = request.method
+    if method == "GET":
+        return render_template("Day03_03-release.html")
+    else:
+        blogtitle = request.form.get("blogtitle")
+        blogtype = request.form.getlist("blogtype")
+        blogimg = request.files.get("blogimg")
+        blogcontent = request.form.get("blogcontent")
+        print(blogtitle, blogtype, blogcontent)
+        if blogimg != None:
+            basedir = os.path.dirname(__file__)
+            ftime = datetime.datetime.now().strftime("%Y%m%d%H%M%S%f")
+            houzhui = blogimg.filename.split(".")[-1]
+            filename = ftime + "." + houzhui
+            uploaddir = os.path.join(basedir, "static", "upload", filename)
+            blogimg.save(uploaddir)
+    return "上传成功！"
 
 
 if __name__ == "__main__":
