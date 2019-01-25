c7891ec97cbb1a62340cc38ca7b50058cfb1042a
root <root>
2019-01-25 11:28:53 +0800

    sdsd

--- Flask/Day04/run.py
+++ Flask/Day04/run.py
@@ -17,5 +17,24 @@ def form_view():
         return "接收数据成功"
 
 
+# 目的 文件上传
+@app.route("/02-file", methods=["GET", "POST"])
+def file_view():
+    method = request.method
+    if method == "GET":
+        return render_template("Day03_02-file.html")
+    else:
+        # 接收数据
+        uname = request.form.get("uname")
+        uimg = request.files['uimg']  # uimg是一个文件 传入'uimg' 用于传入name=uimg的那个文本框
+        # 保存数据 相对路径satic中
+        try:
+            uimg.save("static/" + uimg.filename)
+        except Exception as e:
+            print(e)
+            return "上传失败！"
+    return "上传文件成功"
+
+
 if __name__ == "__main__":
     app.run(debug=True)
--- Flask项目-博客/run.py
+++ Flask项目-博客/run.py
@@ -2,6 +2,8 @@ from flask import Flask, request
 from flask import render_template
 from werkzeug.security import generate_password_hash, check_password_hash
 
+# generate_password_hash 加密
+# check_password_hash 解密
 app = Flask(__name__)
 
 
@@ -23,8 +25,9 @@ def longin():
     else:
         uname = request.form.get("username")
         pwd = request.form.get("password")
-        print(uname, pwd)
-        return ""
+        pwd2 = generate_password_hash(pwd)
+        print(uname, pwd, pwd2)
+        return "接收数据成功"
 
 
 @app.route("/register", methods=["POST", "GET"])
@@ -37,9 +40,12 @@ def register():
         email = request.form.get("email")
         url = request.form.get("url")
         password = request.form.get("password")
-
-        print(username, email, url, password)
-        return ""
+        password2 = generate_password_hash(password)
+        print(username, email, url, password, password2)
+        # 测试加密字符串和原始字符串是否相同
+        # password2 从注册时候保存在数据库中的拿出来 password 输入的 就可比对
+        print(check_password_hash(password2, password))
+        return "接收数据成功"
 
 
 
