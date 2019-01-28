27840fa2849f498f14f60a5c69d2f5023fa191f8
root <root>
2019-01-28 21:49:46 +0800

    sdsd

--- Flask项目-博客/run.py
+++ Flask项目-博客/run.py
@@ -1,34 +1,40 @@
-from flask import Flask, request
-from flask import render_template
+from flask import Flask, render_template, request
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_script import Manager
-import pymysql
 from flask_sqlalchemy import SQLAlchemy
-
-pymysql.install_as_MySQLdb()
 from flask_migrate import Migrate, MigrateCommand
+
 
 # generate_password_hash 加密
 # check_password_hash 解密
 app = Flask(__name__)
-app.config['DEBUG'] = True;
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://root:root@127.0.0.1:3306/blog"
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:root@localhost:3306/blog"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
 manager = Manager(app)
 migrate = Migrate(app, db)
 manager.add_command('db', MigrateCommand)
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-manager = Manager(app)
 
 
 # 创建实体类
 class Users(db.Model):
-    __tablename__ = "users"
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(200), nullable=True)
-    email = db.Column(db.String(200), nullable=True)
-    url = db.Column(db.String(200), nullable=True)
-    upwd = db.Column(db.String(200), nullable=True)
+    username = db.Column(
+        db.String(30), nullable=False
+    )
+    email = db.Column(
+        db.String(200), nullable=True
+    )
+    url = db.Column(
+        db.String(200), nullable=True
+    )
+    upwd = db.Column(
+        db.String(200), nullable=False
+    )
+
 @app.route("/")
 def index():
     return render_template('index.html')
@@ -72,6 +78,4 @@ def register():
 
 
 if __name__ == "__main__":
-    # pass
-    # app.run(debug=True, host="0.0.0.0")
     manager.run()
