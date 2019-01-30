448c814b680e3b20a81dfd742dd5b7154c838bc3
root <root>
2019-01-30 20:06:05 +0800

    sdsd

--- Flask/Day07/rundb.py
+++ Flask/Day07/rundb.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, render_template
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
 from flask_sqlalchemy import SQLAlchemy
@@ -33,6 +33,41 @@ class Users(db.Model):
 
     def __repr__(self):
         return "<User:%r>" % self.username
+
+
+class Student(db.Model):
+    __tablename__ = 'student'
+    id = db.Column(db.Integer, primary_key=True)
+    sname = db.Column(db.String(30), nullable=False)
+    sage = db.Column(db.Integer, nullable=False)
+    isActive = db.Column(db.Boolean, nullable=False, default=True)
+
+
+class Teacher(db.Model):
+    __tablename__ = 'teacher'
+    id = db.Column(db.Integer, primary_key=True)
+    tname = db.Column(db.String(30), nullable=False)
+    tage = db.Column(db.Integer, nullable=False)
+    # 准备增加对Course（一）的外键引用
+    course_id = db.Column(db.Integer, db.ForeignKey("course.id"), nullable=True)  # 是表名不是类名
+
+
+class Course(db.Model):
+    # 省略__tablename__,默认映射的表名就是将类变成全小写
+    id = db.Column(db.Integer, primary_key=True)
+    cname = db.Column(db.String(30), nullable=False)
+    # 增加关联属性和反向引用关系属性
+    teachers = db.relationship(  # 在Couse中通过teachers 找到所有能教这门课的teacher
+        "Teacher", backref="course",  # 在Teacher中通过course来找到对应的Course
+        lazy="dynamic"
+    )
+
+
+class Wife(db.Model):
+    __tablename__ = 'wife'
+    id = db.Column(db.Integer, primary_key=True)
+    wname = db.Column(db.String(30), nullable=False)
+    wage = db.Column(db.Integer)
 
 
 @app.route("/01-orderby")
@@ -110,6 +145,71 @@ def deletelist():
         db.session.delete(u)
     db.session.commit()
     return "deletelist OK"
+
+
+# 目的 关联数据的插入 增加teacher时同时制定对应的 course
+@app.route("/01-regtea")
+def regtea_views():
+    # 声明一个teacher 的对象 通过course_id属性类关联对应的course
+    tea = Teacher()
+    tea.tname = "魏明择"
+    tea.tage = 40
+    tea.course_id = 1
+    db.session.add(tea)
+
+    # 方案2 声明一个Teacher对象 通过course属性来关联对应的Course(反向引用)
+    # 1 获取cname为爬虫的course 对象
+    course = Course.query.filter_by(cname="爬虫").first()
+    # 2 创建Teacher对象 并关联course 对象
+    teacher = Teacher()
+    teacher.tname = "王伟超"
+    teacher.tage = 37
+    teacher.course = course
+    # teacher.course_id=course.id #因为你告诉了teacher.course_id ref course.id
+    # course_id = db.Column(db.Integer, db.ForeignKey("course.id"), nullable=True)  # 是表名不是类名
+    db.session.add(teacher)
+    return "Teacher注册成功"
+
+
+# 目的 一对多 关联属性 反向引用关系的相关查询
+@app.route("/02-query")
+def query_views():
+    # 查询 王伟超老师 的个人信息以及相应的课程信息
+    tea = Teacher.query.filter_by(tname="王伟超").first()
+    print("tea.course的类型为:", type(tea.course))  # teacher中有course 是因为在关联属性的时候设定进去的
+    print("姓名 ：%s ,年龄: %s 所教课程:%s" % (tea.tname, tea.tage, tea.course.cname))
+
+    # 查询爬虫课程以及对应的授课老师
+    course = Course.query.filter_by(cname="爬虫").first()
+    print("课程名称:" + course.cname)
+    teas = course.teachers
+    print(type(teas))  # AppenderBaseQuery 是一个query 可以进一步查询
+    # course.teachers 是针对course课程的对应的所有授课老师的一个查询对象（并非最终结果）
+    for t in teas.all():
+        print("老师名称：", t.tname)
+        print("老师年龄：", t.tage)
+    return "Query OK"
+
+
+@app.route("/03-exec")
+def getteacher():
+    # 查询Course实体中所有的数据
+    courses = Course.query.all()
+
+    # 接收前端传递过来的cid的值
+    # 有cid参数,则查询对应的课程的老师
+    # 没有cid参数,则查询所有老师
+    if 'cid' in request.args:
+        cid = request.args['cid']
+        # 先查询出cid对应的课程
+        course = Course.query.filter_by(id=cid).first()
+        # 再查询课程对应的老师们
+        teachers = course.teachers.all()
+    else:
+        cid = "0"
+        teachers = Teacher.query.all()
+
+    return render_template('Day07_01-teachers.html', courses=courses, teachers=teachers, cid=int(cid))
 
 
 if __name__ == '__main__':
