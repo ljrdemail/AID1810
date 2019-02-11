1b45c25d8c2711ba54c1289303867353d66b7207
root <root>
2019-02-11 16:21:46 +0800

    sdsd

--- Ajax/Day01/AjaxDemo01.py
+++ Ajax/Day01/AjaxDemo01.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
@@ -10,7 +10,14 @@ def index():
 
 @app.route("/01-server")
 def server01():
+    # return "这是我的第一个AJAX请求"+100 #引起500错误
     return "这是我的第一个AJAX请求"
+
+
+@app.route("/02-server")
+def server02():
+    uname = request.args.get("uname")
+    return "欢迎：" + uname
 
 
 if __name__ == "__main__":
