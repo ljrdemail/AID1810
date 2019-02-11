0acbbaeaa50b6b35f2e91c71e1aa2792f41f0d26
Gary <ljrdemail@sohu.com>
2019-02-11 22:26:22 +0800

    撒大声地所多

--- Ajax/Day01/HomeWork.py
+++ Ajax/Day01/HomeWork.py
@@ -23,13 +23,39 @@ migrate = Migrate(app, db)
 manager.add_command('db', MigrateCommand)
 
 
-class User(db.Model):
-    __table__name = "users"
+class Users(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, index=True)
+    password = db.Column(db.String(100), unique=True)
+    email = db.Column(db.String(120), unique=True)
+    isActive = db.Column(db.Boolean, default=True)
 
 
 @app.route('/')
 def index():
     return "Hello World"
+
+
+@app.route("/user-query", methods=["GET", "POST"])
+def user_query():
+    uname = request.args.get("uname")
+    res = db.session.query(Users).filter(Users.username == uname)
+    if not res:
+        return "用户已经存在，请重新输入用户名！"
+
+
+@app.route("/save-user", methods=["GET", "POST"])
+def save_user():
+    uname = request.form.get("uname")
+    upwd = request.form.get("upwd")
+    uemail = request.form.get("uemail")
+    user = Users
+    user.username = uname
+    user.password = upwd
+    user.email = uemail
+    db.session.add(user)
+    return "注册成功"
 
 
 @app.route("/register", methods=["POST"])
@@ -38,4 +64,4 @@ def register():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    manager.run()
