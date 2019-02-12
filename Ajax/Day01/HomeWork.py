196deed36009582696c915c2c44dee4f0fac6200
root <root>
2019-02-12 11:32:21 +0800

    sdsd

--- Ajax/Day01/HomeWork.py
+++ Ajax/Day01/HomeWork.py
@@ -6,7 +6,7 @@ from sqlalchemy import func
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:root@localhost:3306/flask"
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:root@localhost:3306/ajax"
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -27,8 +27,8 @@ class Users(db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, index=True)
-    password = db.Column(db.String(100), unique=True)
-    email = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(100))
+    email = db.Column(db.String(120))
     isActive = db.Column(db.Boolean, default=True)
 
 
@@ -40,9 +40,11 @@ def index():
 @app.route("/user-query", methods=["GET", "POST"])
 def user_query():
     uname = request.args.get("uname")
-    res = db.session.query(Users).filter(Users.username == uname)
-    if not res:
+    res = db.session.query(Users.id, func.count('*')).filter(Users.username == uname)
+    if res[0][1] == 1:
         return "用户已经存在，请重新输入用户名！"
+    else:
+        return "用户不存在，可以注册！"
 
 
 @app.route("/save-user", methods=["GET", "POST"])
@@ -50,17 +52,12 @@ def save_user():
     uname = request.form.get("uname")
     upwd = request.form.get("upwd")
     uemail = request.form.get("uemail")
-    user = Users
+    user = Users()
     user.username = uname
     user.password = upwd
     user.email = uemail
     db.session.add(user)
     return "注册成功"
-
-
-@app.route("/register", methods=["POST"])
-def register():
-    pass
 
 
 if __name__ == "__main__":
