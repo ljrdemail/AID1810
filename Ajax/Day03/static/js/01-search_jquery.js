0d1ddbdf13d9a9cfc73dcda5a30f6dc1571cacde
root <root>
2019-02-13 14:14:37 +0800

    sdsd

--- Ajax/Day03/static/js/01-search_jquery.js
+++ Ajax/Day03/static/js/01-search_jquery.js
@@ -1,20 +1,17 @@
 $(function () {
     $("#uname").keyup(function () {
-        $.get("/01-search", "uname=" + this.value, function (resText) {
-
-            if (resText.length > 0) {
+        $.get("/01-search", "uname=" + this.value, function (data) {
+            //data 是相应回来的数据 直接被转换成js数据
+            if (data.length > 0) {
                 $("#show").html("")
                 $("#show").css("display", "block")
                 $.each(resText, function (i, obj) {
                     var $p = $("<p>" + obj + "</p>")
                     $("#show").append($p)
                 })
-
             } else {
                 $("#show").css("display", "none")
             }
-
-
         }, "json")
 
 
