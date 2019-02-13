3de9c59cc929a3c610bb97e43f7cbeb11593f518
root <root>
2019-02-13 19:45:05 +0800

    sdsd

---
+++ Ajax/Day03/citys.sql
@@ -0,0 +1,8 @@
+INSERT INTO `citys` VALUES (1, '广州', 1);
+INSERT INTO `citys` VALUES (2, '深圳', 1);
+INSERT INTO `citys` VALUES (3, '长沙', 2);
+INSERT INTO `citys` VALUES (4, '怀化', 2);
+INSERT INTO `citys` VALUES (5, '西安', 3);
+INSERT INTO `citys` VALUES (6, '铜川', 3);
+INSERT INTO `citys` VALUES (7, '渭南', 3);
+INSERT INTO `citys` VALUES (8, '河源', 1);
---
+++ Ajax/Day03/provinces.sql
@@ -0,0 +1,3 @@
+INSERT INTO `provinces` VALUES (1, '广东');
+INSERT INTO `provinces` VALUES (2, '湖南');
+INSERT INTO `provinces` VALUES (3, '陕西');
