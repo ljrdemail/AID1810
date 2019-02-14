c12757e70174ffe1c3f1f164ef9b97106909fe8e
root <root>
2019-02-14 16:13:19 +0800

    sdsd

---
+++ Flask项目-博客_项目结构改造/app/__init__.py
@@ -0,0 +1,29 @@
+# 当前程序的初始化
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+# 声明 SQLAlchemy的实例
+db = SQLAlchemy()  # 放在外面是为了便于别的模块导入db 不用new App 之后返回db
+
+
+# 1 创建flask 应用(app) 以及各种配置
+def create_app():
+    # 创建Flask程序示例-App
+    app = Flask(__name__)
+    # 为app指定各种配置
+    app.config['DEBUG'] = True
+    # 为app指定数据库相关配置
+    app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:root@localhost:3306/blog"
+    app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    # 配置session所需要的SECURET_KEY
+    app.config["SECURET_KEY"] = "ljrdemail5266790"  # 随便写用于密码加密
+    # 关联db 以及 app
+    db.init_app(app)  # 指定db 用哪个App初始化
+    # 返回创建好的程序实例app
+    return app
+
+
+# 2 创建SQLAlchemy的应用实例（db）
+def create_db():
+    pass
---
+++ Flask项目-博客_项目结构改造/manage.py
@@ -0,0 +1,28 @@
+# 启动和管理项目的操作代码
+# 导入app 中的create_app 用于启动
+from app import create_app, db
+# 通过manager()管理项目 并增加数据迁移指令
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager
+# 导入所有的实体类方便使用db指令做迁移
+from app.models import *
+
+# 调用create _app得到app
+app = create_app()
+# 创建Manager 实例用于托管app
+manager = Manager(app)
+# 创建Migrate对象用于关联要管理的app和db
+migrate = Migrate(app, db)
+# 再通过Manager对象增加db 迁移指令
+manager.add_command('db', MigrateCommand)
+
+if __name__ == "__main__":
+    # app.run()
+    # 通过manager 实例来启动程序
+    manager.run()
+
+# 在Terminal中在项目目录下执行以下命令
+# 1 python manage.py db init
+# 2 python manage.py db migrate
+# 3 python manage.py db upgrade
+# 会在项目目录下添加 migrations 子目录并在数据中添加alembic_version表
