f48efee3fc7fd8c75d1535f592036cd8e0a19f73
root <root>
2019-02-15 11:00:41 +0800

    sdsd

--- Flask项目-博客_项目结构改造/app/main/views.py
+++ Flask项目-博客_项目结构改造/app/main/views.py
@@ -4,9 +4,11 @@ from flask import render_template
 from ..models import *
 from .. import db
 
+
 @main.route('/')
 def main_index():
     # 查询Category中所有的分类信息
     categories = Category.query.all()
+    topics = Topic.query.all()
 
     return render_template('index.html', params=locals())
