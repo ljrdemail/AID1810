493e45ac3433de132de8d9770b51a6ae9e5365e2
root <root>
2019-02-16 16:57:38 +0800

    sdsd

--- OOP/Day04/MyList2.py
+++ OOP/Day04/MyList2.py
@@ -30,6 +30,6 @@ print("程序结束")
 
 L1 = MyList([1, 2, 3])
 print(id(L1)) #调用  return repr(self.data)的ID
-L1 *= 2  # L1=L1*2 如果有inul 优先调用 没有拆解为L1*22 调用mul
+L1 *= 2  # L1=L1*2 如果有imul 优先调用 没有拆解为L1*22 调用mul
 print("外面的ID为：",id(L1)) # 调用  return MyList(self.data * other) 的ID 也就是向说 *2 之后产生新的列表 id 跟老列表不一样
 print(L1)
--- OOP/Day04/mylist7.py
+++ OOP/Day04/mylist7.py
@@ -13,24 +13,29 @@ class MyList:
             print("用户在做索引操作")
         elif type(item) is slice:
             print("用户在做切片操作")
-            print("起始值为",item.start)
+            print("起始值为", item.start)
             print("终止值为", item.stop)
             print("步长值为", item.step)
-
 
         print("getitem被调用 item=", item)
         return self.data[item]
 
     def __setitem__(self, key, value):
-        print("setitem被调用 key=%r value=%r" %(key,value))
+        print("setitem被调用 key=%r value=%r" % (key, value))
         self.data[key] = value
         return self.data
+
     def __delitem__(self, key):
         print("delitem被调用 key=%r " % (key))
         return self.data.pop(key)
 
 
 L1 = MyList([1, -2, 3, -4, 5])
-#lst=L1[::2] #切片调用的是 getitem
-#print(lst)
-lst=L1[0:10:2]
+# lst=L1[::2] #切片调用的是 getitem
+# print(lst)
+lst = L1[0:10:2]
+print(lst)
+L1[0:2] = [5, 6]
+print(L1)
+del [L1[0]]
+print(L1)
