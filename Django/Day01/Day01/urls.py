11b140562b30537d2c47c74fc215450fbe08dea7
root <root>
2019-02-18 17:14:00 +0800

    sdsd

---
+++ Django/Day01/Day01/urls.py
@@ -0,0 +1,27 @@
+"""Day01 URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.conf.urls import url, include
+from django.contrib import admin
+
+urlpatterns = [
+    url('admin/', admin.site.urls),
+    # 判断访问路径是否为music/开头的
+    url(r'^music/', include("music.urls")),  # 注意music/后没有$ 余下的交给music目录下的urls.py进一步处理
+    url(r'^index/', include("index.urls")),  # 注意index/后没有$ 余下的交给index目录下的urls.py进一步处理
+    url(r'^sport/', include("sport.urls")),  # 注意sport/后没有$ 余下的交给sport目录下的urls.py进一步处理
+    url(r'^news/', include("news.urls")),  # 注意news/后没有$ 余下的交给news目录下的urls.py进一步处理
+
+]
--- DjangoDemo1/sport/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-
-
-# Create your views here.
-
-
-def index(request):
-    return HttpResponse("这是sport应用中的首页")
