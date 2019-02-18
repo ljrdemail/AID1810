a1904822877fce7d543e0750d4b3bec7ff786ce0
root <root>
2019-02-18 16:11:33 +0800

    sdsd

--- DjangoDemo1/DjangoDemo1/urls.py
+++ DjangoDemo1/DjangoDemo1/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url, include
 from django.contrib import admin
-from django.urls import path
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url('admin/', admin.site.urls),
+    # 判断访问路径是否为music/开头的
+    url(r'^music/', include("music.urls"))  # 注意music/后没有$ 余下的交给music目录下的urls.py进一步处理
+
 ]
--- DjangoDemo1/music/views.py
+++ DjangoDemo1/music/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def show_01(request):
+    return HttpResponse("这是music应用中的show01访问路径")
