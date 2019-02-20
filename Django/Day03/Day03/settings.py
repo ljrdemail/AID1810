29c2788fe8545f0a6c45600cb94a4d2345e730c9
Gary <ljrdemail@sina.com>
2019-02-20 13:00:25 +0800

    sdsd

--- Django/Day03/Day03/settings.py
+++ Django/Day03/Day03/settings.py
@@ -74,8 +74,13 @@ WSGI_APPLICATION = 'Day03.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'webdb',
+        'USER': 'root',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': '3306'
+
     }
 }
 
--- Django/Day03/index/models.py
+++ Django/Day03/index/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+# 创建一个实体类 -Publisher 表示出版社信息
+# 注意 主键&自增列在Django ORM 中会自动创建
+# 1 name 出版社的名称 -varchar
+# 2 address  出版社的地址 varchar
+# 3 city 出版社所在的城市varchar
+# 4 country 出版社所在的国家 -varchar
+# 5 website 出版社网址
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=30)
+    address = models.CharField(max_length=100)
+    city = models.CharField(max_length=20)
+    country = models.CharField(max_length=20)
+    website = models.URLField()  # Django帮你封装 底层还是varchar
