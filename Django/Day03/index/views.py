677221869352b8deadc0257a653feec5d148664a
Gary <ljrdemail@sina.com>
2019-02-20 10:52:43 +0800

    sdsd

--- Django/Day03/index/views.py
+++ Django/Day03/index/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
+from django.template import loader
 from django.shortcuts import render
 
+
 # Create your views here.
+
+
+def index(request):
+    return HttpResponse("这是index应用中的首页")
